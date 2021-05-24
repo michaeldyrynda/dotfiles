d2f5373e0cdfc60089fae331b11b12180ff2a901
Michael Dyrynda <michael@dyrynda.com.au>
2021-05-24 10:44:51 +0930

    [fzf] update config

--- fzf.zsh
+++ fzf.zsh
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == *${BREW_BASE_DIR}/opt/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}${BREW_BASE_DIR}/opt/fzf/bin"
+if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "${BREW_BASE_DIR}/opt/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "${BREW_BASE_DIR}/opt/fzf/shell/key-bindings.zsh"
+source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
