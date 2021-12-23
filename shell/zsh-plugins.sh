69399ca61159bdf20a0ae2e3bc802a8bccf42350
Michael Dyrynda <michael@dyrynda.com.au>
2021-12-23 13:38:38 +1030

    [shell] update zsh-plugins installation script

--- shell/zsh-plugins.sh
+++ shell/zsh-plugins.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/jessarcher/zsh-artisan.git ~/.oh-my-zsh/custom/plugins/artisan
+INSTALL_BASE=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
 
+if [ ! -d  "${INSTALL_BASE}/plugins/zsh-autosuggestions" ]; then
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${INSTALL_BASE}/plugins/zsh-autosuggestions
+fi
+
+if [ ! -d  "${INSTALL_BASE}/plugins/zsh-syntax-highlighting" ]; then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${INSTALL_BASE}/plugins/zsh-syntax-highlighting
+fi
+
+if [ ! -d  "${INSTALL_BASE}/plugins/artisan" ]; then
+    git clone https://github.com/jessarcher/zsh-artisan.git ${INSTALL_BASE}/plugins/artisan
+fi
