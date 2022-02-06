413ea2615a64ae40514473ec00863782e1d831d6
Michael Dyrynda <michael@dyrynda.com.au>
2022-02-06 21:57:27 +1030

    [dotbot] add install of vue-language-server (vls)

--- nvim/config/lsp.lua
+++ nvim/config/lsp.lua
@@ -5,6 +5,8 @@ local lspconfig = require('lspconfig')
 
 lspconfig.intelephense.setup { }
 
+lspconfig.vuels.setup { }
+
 lspconfig.html.setup({
     cmd = { 'html-languageserver', '--stdio' },
     filetypes = { 'html', 'blade' }
