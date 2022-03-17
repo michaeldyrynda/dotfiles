f237c5d07ebe47ee0d663a6d47397a81601f1fad
Michael Dyrynda <michael@dyrynda.com.au>
2022-03-18 09:31:12 +1030

    [nvim] remove default fzf extension options /ht @jesseleite

--- nvim/config/telescope.lua
+++ nvim/config/telescope.lua
@@ -53,12 +53,7 @@ telescope.setup {
   },
 
   extensions = {
-    fzf = {
-      fuzzy = true,
-      override_generic_sorter = true,
-      override_file_sorter = true,
-      case_mode = "smart_case",
-    }
+    fzf = { }
   }
 }
 
