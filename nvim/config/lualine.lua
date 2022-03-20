550bd209305590d1d63daa947fa14bc276775bfa
Michael Dyrynda <michael@dyrynda.com.au>
2022-03-21 10:28:18 +1030

    [nvim] set globalstatus = true on lualine setup

--- nvim/config/lualine.lua
+++ nvim/config/lualine.lua
@@ -14,6 +14,7 @@ local colors = {
 
 require('lualine').setup {
     options = {
+        globalstatus = true,
         component_separators = { left = '', right = ''},
         section_separators = { left = '', right = ''},
         disabled_filetypes = {},
