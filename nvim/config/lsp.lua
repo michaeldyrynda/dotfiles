d608367559bcd9c76bbad8835265a9b96115ea7c
Michael Dyrynda <michael@dyrynda.com.au>
2023-01-26 16:11:44 +1030

    redefine diagnostic symbols

--- nvim/config/lsp.lua
+++ nvim/config/lsp.lua
@@ -36,3 +36,8 @@ lspconfig.html.setup({
 lspconfig.tailwindcss.setup {
     capabilities = capabilities,
 }
+
+vim.fn.sign_define('DiagnosticSignError', { text = '', texthl = 'DiagnosticSignError' })
+vim.fn.sign_define('DiagnosticSignWarn', { text = '', texthl = 'DiagnosticSignWarn' })
+vim.fn.sign_define('DiagnosticSignInfo', { text = '', texthl = 'DiagnosticSignInfo' })
+vim.fn.sign_define('DiagnosticSignHint', { text = '', texthl = 'DiagnosticSignHint' })
