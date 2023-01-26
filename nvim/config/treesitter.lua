5cf70cb460527b269adce9b976dc675c7c808daa
Michael Dyrynda <michael@dyrynda.com.au>
2023-01-26 16:19:52 +1030

    add treesitter config, allowing colours

---
+++ nvim/config/treesitter.lua
@@ -0,0 +1,24 @@
+require('nvim-treesitter.configs').setup({
+  highlight = {
+    enable = true,
+    disable = { 'NvimTree' },
+    additional_vim_regex_highlighting = true,
+  },
+  textobjects = {
+    select = {
+      enable = true,
+      lookahead = true,
+      keymaps = {
+        ["if"] = "@function.inner",
+        ["af"] = "@function.outer",
+        ["ic"] = "@class.inner",
+        ["ac"] = "@class.outer",
+        ['ia'] = '@parameter.inner',
+        ['aa'] = '@parameter.outer',
+      },
+    },
+  },
+  context_commentstring = {
+    enable = true,
+  },
+})
