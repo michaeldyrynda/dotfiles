b40ce807934e35ef3968735c5e0f0b8fd9298ea6
Michael Dyrynda <michael@dyrynda.com.au>
2023-01-26 16:19:41 +1030

    move vim-test config to lua

---
+++ nvim/config/vim-test.lua
@@ -0,0 +1,16 @@
+vim.keymap.set('n', '<Leader>tn', ':TestNearest<CR>')
+vim.keymap.set('n', '<Leader>tf', ':TestFile<CR>')
+vim.keymap.set('n', '<Leader>ts', ':TestSuite<CR>')
+vim.keymap.set('n', '<Leader>tl', ':TestLast<CR>')
+vim.keymap.set('n', '<Leader>tv', ':TestVisit<CR>')
+
+vim.cmd([[
+let test#php#phpunit#executable = 'deliver vendor/bin/phpunit'
+  let test#php#phpunit#options = '--colors=always'
+  function! FloatermStrategy(cmd)
+    execute 'silent FloatermKill scratch'
+    execute 'FloatermNew! --autoclose=2 --name=scratch '.a:cmd.' |less -X'
+  endfunction
+  let g:test#custom_strategies = {'floaterm': function('FloatermStrategy')}
+  let g:test#strategy = 'floaterm'
+]])
