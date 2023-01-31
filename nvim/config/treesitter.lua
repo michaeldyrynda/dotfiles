60f719a282efb94d75c6e3c3d47349625c8f648c
Michael Dyrynda <michael@dyrynda.com.au>
2023-01-31 11:47:56 +1030

    [nvim] tweak treesitter config

--- nvim/config/treesitter.lua
+++ nvim/config/treesitter.lua
@@ -1,23 +1,29 @@
 require('nvim-treesitter.configs').setup({
-  ensure_installed = 'all',
-  highlight = {
-    enable = true,
-  },
-  textobjects = {
-    select = {
-      enable = true,
-      lookahead = true,
-      keymaps = {
-        ["if"] = "@function.inner",
-        ["af"] = "@function.outer",
-        ["ic"] = "@class.inner",
-        ["ac"] = "@class.outer",
-        ['ia'] = '@parameter.inner',
-        ['aa'] = '@parameter.outer',
-      },
+    context_commentstring = {
+        enable = true,
     },
-  },
-  context_commentstring = {
-    enable = true,
-  },
+    ensure_installed = 'all',
+    highlight = {
+        enable = true,
+    },
+    indent = {
+        enable = true,
+    },
+    playground = {
+        enable = true,
+    },
+    textobjects = {
+        select = {
+        enable = true,
+        lookahead = true,
+        keymaps = {
+            ["if"] = "@function.inner",
+            ["af"] = "@function.outer",
+            ["ic"] = "@class.inner",
+            ["ac"] = "@class.outer",
+            ['ia'] = '@parameter.inner',
+            ['aa'] = '@parameter.outer',
+        },
+        },
+    },
 })
