05d1b6c2d3a6c9b6c13de6a9f54cf891be21628a
Michael Dyrynda <michael@dyrynda.com.au>
2023-05-14 07:10:38 +0930

    [vim] remove stray on_attach

--- nvim/config/lsp.lua
+++ nvim/config/lsp.lua
@@ -29,7 +29,6 @@ lspconfig.emmet_ls.setup({
 
 lspconfig.intelephense.setup({ 
     on_attach = function(client, bufnr)
-        on_attach(client, bufnr)
         client.server_capabilities.documentFormattingProvider = false
         client.server_capabilities.documentRangeFormattingProvider = false
     end,
