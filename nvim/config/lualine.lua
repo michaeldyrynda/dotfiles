7e0108992586f29ebe3dbe89bb70f928cb235b59
Michael Dyrynda <michael@dyrynda.com.au>
2023-06-05 08:41:43 +0930

    [vim] lualine config

--- nvim/config/lualine.lua
+++ nvim/config/lualine.lua
@@ -1,6 +1,8 @@
+local separator = { '"▏"', color = 'StatusLineNonText' }
+
 require('lualine').setup({
   options = {
-    component_separators = { left = '', right = ''},
+    component_separators = { left = '', right = ''},
     globalstatus = true,
     theme = {
       normal = {
@@ -13,23 +15,27 @@ require('lualine').setup({
   sections = {
     lualine_a = {
       'mode',
+      separator
     },
     lualine_b = {
       'branch',
       'diff',
-      '"  " .. tostring(#vim.tbl_keys(vim.lsp.buf_get_clients()))',
+      separator,
+      'tostring(#vim.tbl_keys(vim.lsp.buf_get_clients()))',
       { 'diagnostics', sources = { 'nvim_diagnostic' } },
     },
     lualine_c = {
       'filename'
     },
-    lualine_x = {
-      'filetype',
+     lualine_x = {
       'encoding',
+      'filetype',
       'fileformat',
     },
     lualine_y = {
+      separator,
       '(vim.bo.expandtab and "␠ " or "⇥ ") .. " " .. vim.bo.shiftwidth',
+      separator,
     },
     lualine_z = {
       'location',
