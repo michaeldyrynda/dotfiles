b1d71b7716ddd2aede002172c5f7980bb71564f3
Michael Dyrynda <michael.dyrynda@lmg.broker>
2023-08-10 10:00:24 +0930

    [vim] remove extra diagnostic display

--- nvim/config/lsp.lua
+++ nvim/config/lsp.lua
@@ -10,12 +10,6 @@ vim.diagnostic.config({
     float = {
         source = true,
         focus = false,
-        format = function (diagnostic)
-            if diagnostic.user_data ~= nil and diagnostic.user_data.lsp.code ~= nil then
-                return string.format('%s: %s', diagnostic.user_data.lsp.code, diagnostic.message)
-            end
-            return diagnostic.message
-        end,
     }
 })
 
