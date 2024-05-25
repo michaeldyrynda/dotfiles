9467a41f935dcfe3fcc4951cad7957aa3dd035c4
Michael Dyrynda <michael@dyrynda.com.au>
2024-05-25 13:04:25 +0930

    [nvim] tweak phpactor LSP configuration

--- nvim/config/lsp.lua
+++ nvim/config/lsp.lua
@@ -26,7 +26,9 @@ lspconfig.intelephense.setup({
         client.server_capabilities.documentFormattingProvider = false
         client.server_capabilities.documentRangeFormattingProvider = false
     end,
+
     capabilities = capabilities,
+
     settings = {
         intelephense = {
             diagnostics = {
@@ -38,11 +40,71 @@ lspconfig.intelephense.setup({
 
 lspconfig.phpactor.setup {
     capabilities = capabilities,
+
+    on_attach = function(client, bufnr)
+        client.server_capabilities.completionProvider = false
+        client.server_capabilities.hoverProvider = false
+        client.server_capabilities.implementationProvider = false
+        client.server_capabilities.referencesProvider = false
+        client.server_capabilities.renameProvider = false
+        client.server_capabilities.selectionRangeProvider = false
+        client.server_capabilities.signatureHelpProvider = false
+        client.server_capabilities.typeDefinitionProvider = false
+        client.server_capabilities.workspaceSymbolProvider = false
+        client.server_capabilities.definitionProvider = false
+        client.server_capabilities.documentHighlightProvider = false
+        client.server_capabilities.documentSymbolProvider = false
+        client.server_capabilities.documentFormattingProvider = false
+        client.server_capabilities.documentRangeFormattingProvider = false
+    end,
+
+    init_options = {
+        ["language_server_worse_reflection.inlay_hints.enable"] = true,
+        ["language_server_worse_reflection.inlay_hints.params"] = true,
+        ["language_server_worse_reflection.inlay_hints.types"] = false,
+    },
+
+    handlers = {
+        ['textDocument/publishDiagnostics'] = function() end
+    }
 }
 
 lspconfig.vuels.setup {
     capabilities = capabilities,
 }
+
+lspconfig.tsserver.setup ({
+    capabilities = capabilities,
+
+    root_dir = function(...)
+        return require("lspconfig.util").root_pattern(".git")(...)
+    end,
+    single_file_support = false,
+    settings = {
+        typescript = {
+            inlayHints = {
+                includeInlayParameterNameHints = "literal",
+                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayVariableTypeHints = false,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayEnumMemberValueHints = true,
+            },
+        },
+        javascript = {
+            inlayHints = {
+                includeInlayParameterNameHints = "all",
+                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayVariableTypeHints = true,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayEnumMemberValueHints = true,
+            },
+        },
+    },
+})
 
 lspconfig.sqlls.setup {
     capabilities = capabilities,
