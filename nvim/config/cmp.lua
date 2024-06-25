494d94ef528e8149aa8b9b78be0a9b5af19498fe
Michael Dyrynda <michael@dyrynda.com.au>
2024-06-25 20:57:53 +0930

    [nvim] formatting and config updates for nvim-cmp

--- nvim/config/cmp.lua
+++ nvim/config/cmp.lua
@@ -3,38 +3,23 @@ local luasnip = require('luasnip')
 local lspkind = require('lspkind')
 local compare = require('cmp.config.compare')
 
-
 local has_words_before = function()
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
 cmp.setup({
-    formatting = {
-        format = lspkind.cmp_format({
-            with_text = true,
-            menu = {
-                nvim_lsp = '[LSP]',
-                buffer = '[BUF]',
-            },
-        }),
-    },
-
-    sorting = {
-        comparators = {
-            cmp.config.compare.score,
-            cmp.config.compare.locality,
-            cmp.config.compare.order,
-            cmp.config.compare.recently_used,
-            cmp.config.compare.offset,
-        }
+    snippet = {
+        expand = function (args)
+            require("luasnip").lsp_expand(args.body)
+        end
     },
 
     mapping = cmp.mapping.preset.insert({
         ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-u>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.close(),
+        ['<C-e>'] = cmp.mapping.abort(),
         ['<C-y>'] = cmp.mapping.confirm({
             behavior = cmp.ConfirmBehavior.Insert,
             select = true,
@@ -62,25 +47,33 @@ cmp.setup({
         end, { "i", "s" }),
     }),
 
-    window = {
-      completion = cmp.config.window.bordered(),
-      documentation = cmp.config.window.bordered(),
-    },
-
-    snippet = {
-        expand = function (args)
-            require("luasnip").lsp_expand(args.body)
-        end
-    },
-
     sources = {
-        { name = 'gh_issues', priority = 60 },
-        { name = 'nvim_lsp', priority = 50 },
-        { name = 'nvim_lsp_signature_help', priority = 40 },
-        { name = 'luasnip', priority = 30 },
-        { name = 'path', priority = 20 },
-        { name = 'buffer', priority = 10 },
-    }
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+        { name = 'nvim_lsp_signature_help' },
+        { name = 'path' },
+        { name = 'buffer' },
+    },
+
+    formatting = {
+        format = lspkind.cmp_format({
+            with_text = true,
+            menu = {
+                nvim_lsp = '[LSP]',
+                buffer = '[BUF]',
+            },
+        }),
+    },
+
+    sorting = {
+        comparators = {
+            cmp.config.compare.score,
+            cmp.config.compare.locality,
+            cmp.config.compare.order,
+            cmp.config.compare.recently_used,
+            cmp.config.compare.offset,
+        }
+    },
 })
 
 require('luasnip/loaders/from_vscode').load({
