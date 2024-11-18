1cbb8333769c5a3e7734294b3739eba1445c46b4
Michael Dyrynda <michael@dyrynda.com.au>
2024-11-18 12:28:04 +1030

    [nvim] remove neoscroll

--- nvim/config/neoscroll.lua
+++ nvim/config/neoscroll.lua
@@ -1,6 +1,1 @@
 require('neoscroll').setup()
-
-require('neoscroll.config').set_mappings({
-  ['<C-u>'] = { 'scroll', { '-vim.wo.scroll', 'true', '50' } },
-  ['<C-d>'] = { 'scroll', { 'vim.wo.scroll', 'true', '50' } },
-})
