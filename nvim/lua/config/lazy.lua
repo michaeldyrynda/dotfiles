c17e8872a164fb3c449aa68a7461a9d0d46670ab
Michael Dyrynda <michael@dyrynda.com.au>
2025-01-05 17:04:46 +1030

    [nvim] use tokyonight for lazy.nvim

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -36,7 +36,7 @@ require("lazy").setup({
     },
     -- Configure any other settings here. See the documentation for more details.
     -- colorscheme that will be used when installing plugins.
-    install = { colorscheme = { "habamax" } },
+    install = { colorscheme = { "tokyonight" } },
     -- automatically check for plugin updates
     checker = { enabled = true },
 })
