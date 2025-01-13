9ee01c7063648157f87080cb600eea0137fcf193
Michael Dyrynda <michael@dyrynda.com.au>
2025-01-13 10:58:37 +1030

    [nvim] update phpactor mappings

--- nvim/lua/plugins/phpactor.lua
+++ nvim/lua/plugins/phpactor.lua
@@ -8,9 +8,9 @@ return {
     keys = {
         { '<Leader>p', ':PhpactorContextMenu<CR>' },
         { '<Leader>u', ':call phpactor#ImportClass()<CR>' },
-        { '<leader>ec', ':call phpactor#ExtractConstant()<cr>' },                    -- Extract a constant
-        { '<leader>ee', ':<c-u>call phpactor#ExtractExpression(visualmode())<cr>' }, -- Extract an expression (variable)
-        { '<leader>em', ':<c-u>call phpactor#ExtractMethod(visualmode())<cr>' },     -- Extract a method
+        { '<leader>ec', ':call phpactor#ExtractConstant()<cr>', mode = 'v' },                    -- Extract a constant
+        { '<leader>ee', ':<c-u>call phpactor#ExtractExpression(visualmode())<cr>', mode = 'v' }, -- Extract an expression (variable)
+        { '<leader>em', ':<c-u>call phpactor#ExtractMethod(visualmode())<cr>', mode = 'v' },     -- Extract a method
         { '<Leader>nc', ':PhpactorClassNew<CR>' },
         { '<Leader>mf', ':PhpactorMovileFile<CR>' },
         { '<Leader>fu', ':PhpactorFindReferences<CR>' },
