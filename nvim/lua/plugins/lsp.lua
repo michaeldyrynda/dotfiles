d9e43541f5349a0f46a0d876f341479bea0b06b6
Michael Dyrynda <michael@dyrynda.com.au>
2026-02-16 10:43:21 +1030

    [nvim] enable validation with jsonls

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -105,6 +105,7 @@ return {
             settings = {
                 json = {
                     schemas = require('schemastore').json.schemas(),
+                    validate = { enable = true }
                 },
             },
         })
