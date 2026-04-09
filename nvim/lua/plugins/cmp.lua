633f22084d95e4971ee7e397f7af4a6e8062b496
Michael Dyrynda <michael.dyrynda@lmg.broker>
2026-04-09 10:57:18 +0930

    [nvim] add cmp-cmdline to get completion for local files

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -6,6 +6,7 @@ return {
         'hrsh7th/cmp-nvim-lsp-signature-help',
         'hrsh7th/cmp-buffer',
         'hrsh7th/cmp-path',
+        'hrsh7th/cmp-cmdline',
         'L3MON4D3/LuaSnip',
         'saadparwaiz1/cmp_luasnip',
         'onsails/lspkind-nvim',
