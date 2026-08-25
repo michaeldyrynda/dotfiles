ebfe5520460f89ba423f00563dc361f54e0fa658
Michael Dyrynda <michael@dyrynda.com.au>
2026-08-25 16:12:36 +0930

    [nvim] remove phpactor keymaps covered by LSP
    - context menu, import class, and find references now handled via
      LSP code actions and telescope

--- nvim/lua/plugins/phpactor.lua
+++ nvim/lua/plugins/phpactor.lua
@@ -6,14 +6,11 @@ return {
     ft = 'php',
 
     keys = {
-        { '<Leader>p', ':PhpactorContextMenu<CR>' },
-        { '<Leader>u', ':call phpactor#ImportClass()<CR>' },
-        { '<leader>ec', ':call phpactor#ExtractConstant()<cr>', mode = 'v' },                    -- Extract a constant
-        { '<leader>ee', ':<c-u>call phpactor#ExtractExpression(visualmode())<cr>', mode = 'v' }, -- Extract an expression (variable)
-        { '<leader>em', ':<c-u>call phpactor#ExtractMethod(visualmode())<cr>', mode = 'v' },     -- Extract a method
+        { '<leader>ec', ':call phpactor#ExtractConstant()<cr>', mode = 'v' },
+        { '<leader>ee', ':<c-u>call phpactor#ExtractExpression(visualmode())<cr>', mode = 'v' },
+        { '<leader>em', ':<c-u>call phpactor#ExtractMethod(visualmode())<cr>', mode = 'v' },
         { '<Leader>nc', ':PhpactorClassNew<CR>' },
         { '<Leader>mf', ':PhpactorMoveFile<CR>' },
-        { '<Leader>fu', ':PhpactorFindReferences<CR>' },
-        { '<Leader>pt', ':call phpactor#Transform()<cr>f<cr>' }
+        { '<Leader>pt', ':call phpactor#Transform()<cr>f<cr>' },
     }
 }
